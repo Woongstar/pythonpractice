f35b49b31986bfedfe65e4043b22dd95097d5de5
Jiwoong Yang <69667560+Woongstar@users.noreply.github.com>
2021-05-24 15:57:34 +0900

    Add files via upload

---
+++ python1/11to20.py
@@ -0,0 +1,34 @@
+# 초보자를 위한 파이썬 300제
+#11~20
+
+삼성전자 = 50000
+총평가금액 = 삼성전자*10
+print(총평가금액)
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+s="hello"
+t="python"
+print(s+"!", t)
+
+print(2+2*3)
+
+a=128
+print(type(a))
+
+num_str = "720"
+num_int = int(num_str)
+print(type(num_int))
+
+num = 100
+result = str(num)
+print(result, type(result))
+
+월 = 48584
+총금액 = 월*36
+print(총금액)
