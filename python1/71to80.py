c11c852f71d35f6215e6bb097dad68d8255c380d
Jiwoong Yang <69667560+Woongstar@users.noreply.github.com>
2021-05-30 01:00:30 +0900

    Add files via upload

---
+++ python1/71to80.py
@@ -0,0 +1,17 @@
+# 초보자를 위한 파이썬 300제
+#71~80
+
+my_variable = ()
+
+print(type(my_variable))
+
+movie_rank = ("닥터 스트레인지","스플릿","럭키")
+print(movie_rank)
+
+my_tuple = (1,)
+
+interest = ('삼성전자', 'LG전자', 'SK Hynix')
+print(list(interest))
+
+data = tuple(range(2,100,2))
+print(data)
